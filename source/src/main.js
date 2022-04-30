934a1e07054274ad88d08759cdf6d71db2788a36
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-04-30 08:27:55 +0200

    removing spaces

--- source/src/main.js
+++ source/src/main.js
@@ -1,18 +1,15 @@
+// eslint-disable-next-line
 import { createApp } from 'vue'
-
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import { initializeApp } from 'firebase/app'
-
 import { getPerformance } from 'firebase/performance'
 
 const firebaseConfig = require('./firebase.js')
 
-const app = initializeApp(firebaseConfig.getFirebaseConfig())
-
-getPerformance(app)
+const app = initializeApp( firebaseConfig.getFirebaseConfig() )
+getPerformance( app )
 
 createApp(App)
   .use(store)
