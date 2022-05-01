a1841672fbd5670d21eefc8d6139e0e3c8850b3b
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-01 19:42:33 +0200

    working

--- backend/app/Http/Middleware/TrustHosts.php
+++ backend/app/Http/Middleware/TrustHosts.php
@@ -1,20 +1,22 @@
 <?php
 
-namespace App\Http\Middleware;
+    namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustHosts as Middleware;
+    use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
-class TrustHosts extends Middleware
-{
-    /**
-     * Get the host patterns that should be trusted.
-     *
-     * @return array<int, string|null>
-     */
-    public function hosts()
+    class TrustHosts extends Middleware
     {
-        return [
-            $this->allSubdomainsOfApplicationUrl(),
-        ];
+        /**
+         * Get the host patterns that should be trusted.
+         *
+         * @return array<int, string|null>
+         */
+        public function hosts()
+        {
+            return [
+                $this->allSubdomainsOfApplicationUrl(),
+            ];
+        }
     }
-}
+
+?>
