b31e70701b612ba9111bba07bf18bd3877d2e065
kent vejrup madsen <kent.vejrup.madsen@protonmail.com>
2022-05-08 17:43:38 +0200

    c

--- backend/public/frontend/src/views/AboutView.vue
+++ backend/public/frontend/src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
+  <div class="view-about">
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+  export default 
+  {
+    name: 'AboutView',
+    components: 
+    {
+      
+    }
+  }
+</script>
--- backend/public/frontend/src/views/LoginView.vue
+++ backend/public/frontend/src/views/LoginView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
+  <div class="view-login">
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+  export default 
+  {
+    name: 'LoginView',
+    components: 
+    {
+      
+    }
+  }
+</script>
