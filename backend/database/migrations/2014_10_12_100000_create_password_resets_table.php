f33b3b6829547b0680f368fe24e5e9f27989a518
kent vejrup madsen <kent.vejrup.madsen@protonmail.com>
2022-05-08 17:38:24 +0200

    current

--- backend/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ backend/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
     use Illuminate\Database\Migrations\Migration;
     use Illuminate\Database\Schema\Blueprint;
     use Illuminate\Support\Facades\Schema;
@@ -15,9 +14,14 @@
             Schema::create( 'password_resets', 
                 function (Blueprint $table) 
                 {
-                    $table->bigInteger( 'email_id' )->unsigned()->index();
+                    $table->bigInteger( 'email_id' )
+                          ->unsigned()
+                          ->index();
+
                     $table->string( 'token' );
-                    $table->timestamp( 'created_at' )->nullable();
+                    
+                    $table->timestamp( 'created_at' )
+                          ->nullable();
                 }
             );
         }
@@ -31,35 +35,3 @@
     };
 
 ?>
-=======
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('password_resets');
-    }
-};
->>>>>>> a17ee82b33bb976bbf37d38de9021d162f25cc15
--- backend/public/frontend/src/router/index.js
+++ backend/public/frontend/src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
+const routes = 
+[
   {
     path: '/',
     name: 'home',
@@ -10,16 +11,40 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/register',
+    name: 'register profile',
+    component: () => import(/* webpackChunkName: "about" */ '../views/RegisterView.vue')
+  },
+  {
+    path: '/kanban',
+    name: 'kanban',
+    component: () => import(/* webpackChunkName: "about" */ '../views/KanbanView.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ProfileView.vue')
+  },
+  {
+    path: '/help',
+    name: 'help',
+    component: () => import(/* webpackChunkName: "about" */ '../views/HelpView.vue')
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+const router = createRouter(
+  {
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+  }
+)
 
 export default router
