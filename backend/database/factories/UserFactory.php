251d629249edf4a19ceb5480b2dbe85ae456f61b
kent vejrup madsen <kent.vejrup.madsen@protonmail.com>
2022-05-09 18:36:04 +0200

    reformating code

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -1,6 +1,4 @@
 <?php
-
-<<<<<<< HEAD
     namespace Database\Factories;
 
     use Illuminate\Database\Eloquent\Factories\Factory;
@@ -13,7 +11,8 @@
         
         public function definition()
         {
-            return [
+            return 
+            [
                 'name' => $this->faker->name(),
                 'email' => $this->faker->unique()->safeEmail(),
                 'email_verified_at' => now(),
@@ -25,7 +24,8 @@
         
         public function unverified()
         {
-            return $this->state( function ( array $attributes ) 
+            return $this->state( 
+                function ( array $attributes ) 
                 {
                     return 
                     [
@@ -36,45 +36,3 @@
         }
     }
 ?>
-=======
-namespace Database\Factories;
-
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
-class UserFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
-}
->>>>>>> a17ee82b33bb976bbf37d38de9021d162f25cc15
