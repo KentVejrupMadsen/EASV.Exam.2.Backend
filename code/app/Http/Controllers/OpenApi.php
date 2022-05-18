22dc145ff3f2029f3797c58a08a9cff1e4136558
kent vejrup madsen <kent.vejrup.madsen@protonmail.com>
2022-05-18 22:08:58 +0200

    current

---
+++ code/app/Http/Controllers/OpenApi.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Http\Controllers;
+
+    use OpenApi\Attributes as OA;
+
+
+    #[OA\Info(title: "Kanban-Project-Backend-API", version: "16.05.2022")]
+    class OpenApi
+        extends Controller
+    {
+
+    };
+?>
