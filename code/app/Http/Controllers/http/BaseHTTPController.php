97ead3ab92796606ece18631e56d0c2b92fad47e
kent vejrup madsen <kent.vejrup.madsen@protonmail.com>
2022-05-23 15:02:51 +0200

    added pipeline class

--- code/app/Http/Controllers/http/BaseHTTPController.php
+++ code/app/Http/Controllers/http/BaseHTTPController.php
@@ -2,9 +2,63 @@
     namespace App\Http\Controllers\http;
 
     use App\Http\Controllers\templates\CrudController;
+    use Illuminate\Http\Request;
 
-    use OpenApi\Attributes
-        as OA;
+
+    /**
+     *
+     */
+    interface ControllerPipelineInterface
+    {
+        /**
+         * @param Request $request
+         * @return bool
+         */
+        public function pipelineTowardJSON( Request $request ): bool;
+
+        /**
+         * @return bool
+         */
+        public function hasImplementedJSON(): bool;
+
+
+        /**
+         * @param Request $request
+         * @return bool
+         */
+        public function pipelineTowardXML( Request $request ): bool;
+
+        /**
+         * @return bool
+         */
+        public function hasImplementedXML(): bool;
+
+
+        /**
+         * @param Request $request
+         * @return bool
+         */
+        public function pipelineTowardCSV( Request $request ): bool;
+
+        /**
+         * @return bool
+         */
+        public function hasImplementedCSV(): bool;
+    }
+
+
+    /**
+     *
+     */
+    abstract class ControllerPipeline
+        implements ControllerPipelineInterface
+    {
+        public function __constructor()
+        {
+
+        }
+
+    }
 
 
     /**
@@ -13,12 +67,13 @@
     abstract class BaseHTTPController
         extends CrudController
     {
+        /**
+         *
+         */
         public function __construct()
         {
             parent::__construct();
 
         }
-
-        
     }
 ?>
