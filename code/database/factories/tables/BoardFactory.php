b57350155b42e0f128c9c13dca76abeef99566b9
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-26 23:15:28 +0200

    moving code

--- code/database/factories/tables/BoardFactory.php
+++ code/database/factories/tables/BoardFactory.php
@@ -4,21 +4,15 @@
     use Illuminate\Database\Eloquent\Factories\Factory;
 
 
-    /**
-     * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
-     */
     class BoardFactory
         extends Factory
     {
-        /**
-         * Define the model's default state.
-         *
-         * @return array<string, mixed>
-         */
+
         public function definition()
         {
-            return [
-                //
+            return
+            [
+
             ];
         }
     }
