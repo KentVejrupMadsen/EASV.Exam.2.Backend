38fd7710175ca4f4a7a046c1b2ff86e2b09e08b6
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-27 12:33:30 +0200

    removal

--- code/routes/api.php
+++ code/routes/api.php
@@ -8,7 +8,6 @@
     use App\Http\Controllers\ApiHomeController;
 
     require_once 'api/accountApi.php';
-    require_once 'api/accountEmailApi.php';
     require_once 'api/boardApi.php';
     require_once 'api/kanbanApi.php';
     require_once 'api/projectApi.php';
