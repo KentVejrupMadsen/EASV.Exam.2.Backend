8509b74a23ce88fc86ea7872eadf962c4e8a5df5
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-27 10:09:51 +0200

    database seeding made

--- code/database/seeders/DatabaseSeeder.php
+++ code/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,11 @@
      */
     namespace Database\Seeders;
 
+    use App\Models\tables\AccountEmailModel;
+    use App\Models\tables\User;
+
+    use Carbon\Carbon;
+    use Illuminate\Support\Facades\Hash;
     use Illuminate\Database\Console\Seeds\WithoutModelEvents;
     use Illuminate\Database\Seeder;
 
@@ -20,6 +25,16 @@
 
         public function makeBaseAccount()
         {
+            $modelEmail = AccountEmailModel::factory()->create( [ 'content' => 'unknown' ] );
+
+            User::factory()->create(
+                [ 'username' => 'admin',
+                  'email_id' => $modelEmail->id,
+                  'email_verified_at' => null,
+                  'password'=> Hash::make('admin' ),
+                  'created_at'=>Carbon::now(),
+                  'updated_at'=>Carbon::now(),
+                  'settings'=>'' ] );
 
         }
     }
