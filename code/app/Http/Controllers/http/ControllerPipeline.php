d776c2641a4b11b9e5d2c038e66041ccdc211e99
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-27 12:14:52 +0200

    code splitting

---
+++ code/app/Http/Controllers/http/ControllerPipeline.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App\Http\Controllers\http;
+
+
+    /**
+     * Used to implement a pipeline flow. -> essentially defining which types
+     * can be returned by the API.
+     */
+    abstract class ControllerPipeline
+        implements ControllerPipelineInterface
+    {
+
+
+    }
+?>
---
+++ code/app/Http/Controllers/http/ControllerPipelineInterface.php
@@ -0,0 +1,47 @@
+<?php
+    namespace App\Http\Controllers\http;
+
+    use Illuminate\Http\Request;
+
+
+    /**
+     *
+     */
+    interface ControllerPipelineInterface
+    {
+        /**
+         * @param Request $request
+         * @return bool
+         */
+        public function pipelineTowardJSON( Request $request ): bool;
+
+        /**
+         * @return bool
+         */
+        public function hasImplementedJSON(): bool;
+
+
+        /**
+         * @param Request $request
+         * @return bool
+         */
+        public function pipelineTowardXML( Request $request ): bool;
+
+        /**
+         * @return bool
+         */
+        public function hasImplementedXML(): bool;
+
+
+        /**
+         * @param Request $request
+         * @return bool
+         */
+        public function pipelineTowardCSV( Request $request ): bool;
+
+        /**
+         * @return bool
+         */
+        public function hasImplementedCSV(): bool;
+    }
+?>
