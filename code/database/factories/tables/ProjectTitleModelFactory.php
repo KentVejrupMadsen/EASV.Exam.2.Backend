ad17fd365174803465d76d1b05602e8499f9a447
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-28 14:07:08 +0200

    plausible to run test for projects in the database

--- code/database/factories/tables/ProjectTitleModelFactory.php
+++ code/database/factories/tables/ProjectTitleModelFactory.php
@@ -47,7 +47,7 @@
                 [
                     'content' => $this->faker
                                       ->unique()
-                                      ->jobTitle
+                                      ->realText
                 ];
             }
             else
