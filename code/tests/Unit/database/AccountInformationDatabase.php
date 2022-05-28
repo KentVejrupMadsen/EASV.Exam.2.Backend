a1af00e072a39557563415d3685b2bbdaac3445e
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-28 11:56:46 +0200

    added test for database tables country and zip code

--- code/tests/Unit/database/AccountInformationDatabase.php
+++ code/tests/Unit/database/AccountInformationDatabase.php
@@ -1,6 +1,8 @@
 <?php
     namespace Tests\Unit\database;
 
+    use App\Models\tables\CountryModel;
+    use App\Models\tables\ZipCodeModel;
     use Tests\Unit\BaseUnit;
 
 
@@ -15,6 +17,9 @@
          */
         public function test_add_countries(): void
         {
+            CountryModel::factory()->setDebugState( true );
+            CountryModel::factory()->count(195)->create();
+            CountryModel::factory()->setDebugState( false );
 
             $this->completed();
         }
@@ -25,8 +30,18 @@
          */
         public function test_add_zipCodes(): void
         {
+            $allCountries = CountryModel::all();
+            ZipCodeModel::factory()->setDebugState( true );
 
+            for( $idx = 0;
+                 $idx < sizeof( $allCountries );
+                 $idx++ )
+            {
+                $currentCountry = $allCountries[$idx];
+                ZipCodeModel::factory()->create([ 'country_id' => $currentCountry->id ] );
+            }
 
+            ZipCodeModel::factory()->setDebugState( false );
             $this->completed();
         }
     }
