f9120dea2f777641fcfdcf95c6d0d68fe73b7168
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-28 20:09:01 +0200

    removed testing values

--- resources/sql/trigger.sql
+++ resources/sql/trigger.sql
@@ -1,13 +1,13 @@
 --
 create trigger on_insert_of_account_emails
-    before insert on testing.account_emails
+    before insert on account_emails
     for each row
 begin
     set new.content = lower(new.content);
 end;
 
 create trigger on_update_of_account_emails
-    before update on testing.account_emails
+    before update on account_emails
     for each row
 begin
     set new.content = lower(new.content);
@@ -16,14 +16,14 @@ end;
 
 --
 create trigger on_insert_of_address_road_names
-    before insert on testing.address_road_names
+    before insert on address_road_names
     for each row
 begin
     set new.content = lower(new.content);
 end;
 
 create trigger on_update_of_address_road_names
-    before update on testing.address_road_names
+    before update on address_road_names
     for each row
 begin
     set new.content = lower(new.content);
@@ -32,14 +32,14 @@ end;
 
 --
 create trigger on_insert_of_addresses
-    before insert on testing.addresses
+    before insert on addresses
     for each row
 begin
     set new.levels = lower(new.levels);
 end;
 
 create trigger on_update_of_addresses
-    before update on testing.addresses
+    before update on addresses
     for each row
 begin
     set new.levels = lower(new.levels);
@@ -48,14 +48,14 @@ end;
 
 --
 create trigger on_insert_of_board_titles
-    before insert on testing.board_titles
+    before insert on board_titles
     for each row
 begin
     set new.content = lower(new.content);
 end;
 
 create trigger on_update_of_board_titles
-    before update on testing.board_titles
+    before update on board_titles
     for each row
 begin
     set new.content = lower(new.content);
@@ -64,7 +64,7 @@ end;
 
 -- countries
 create trigger on_insert_of_countries
-    before insert on testing.countries
+    before insert on countries
     for each row
 begin
     set new.country_name = lower(new.country_name);
@@ -72,7 +72,7 @@ begin
 end;
 
 create trigger on_update_of_countries
-    before update on testing.countries
+    before update on countries
     for each row
 begin
     set new.country_name = lower(new.country_name);
@@ -82,14 +82,14 @@ end;
 
 -- kanban title
 create trigger on_insert_of_kanban_titles
-    before insert on testing.kanban_titles
+    before insert on kanban_titles
     for each row
 begin
     set new.content = lower(new.content);
 end;
 
 create trigger on_update_of_kanban_titles
-    before update on testing.kanban_titles
+    before update on kanban_titles
     for each row
 begin
     set new.content = lower(new.content);
@@ -98,14 +98,14 @@ end;
 
 -- Member groups
 create trigger on_insert_of_member_groups
-    before insert on testing.member_groups
+    before insert on member_groups
     for each row
 begin
     set new.content = lower(new.content);
 end;
 
 create trigger on_update_of_member_groups
-    before update on testing.member_groups
+    before update on member_groups
     for each row
 begin
     set new.content = lower(new.content);
@@ -114,14 +114,14 @@ end;
 
 -- Person Firstname
 create trigger on_insert_of_person_first
-    before insert on testing.person_name_first
+    before insert on person_name_first
     for each row
 begin
     set new.content = lower(new.content);
 end;
 
 create trigger on_update_of_person_first
-    before update on testing.person_name_first
+    before update on person_name_first
     for each row
 begin
     set new.content = lower(new.content);
@@ -129,14 +129,14 @@ end;
 
 -- Person Middle and Lastname
 create trigger on_insert_of_person_name_middle_and_last
-    before insert on testing.person_name_middle_and_last
+    before insert on person_name_middle_and_last
     for each row
 begin
     set new.content = lower(new.content);
 end;
 
 create trigger on_update_of_person_name_middle_and_last
-    before update on testing.person_name_middle_and_last
+    before update on person_name_middle_and_last
     for each row
 begin
     set new.content = lower(new.content);
@@ -145,14 +145,14 @@ end;
 
 -- Project titles
 create trigger on_insert_of_project_titles
-    before insert on testing.project_titles
+    before insert on project_titles
     for each row
 begin
     set new.content = lower(new.content);
 end;
 
 create trigger on_update_of_project_titles
-    before update on testing.project_titles
+    before update on project_titles
     for each row
 begin
     set new.content = lower(new.content);
@@ -161,14 +161,14 @@ end;
 
 -- Zip Codes
 create trigger on_insert_of_zip_codes
-    before insert on testing.zip_codes
+    before insert on zip_codes
     for each row
 begin
     set new.area_name = lower(new.area_name);
 end;
 
 create trigger on_update_of_zip_codes
-    before update on testing.zip_codes
+    before update on zip_codes
     for each row
 begin
     set new.area_name = lower(new.area_name);
