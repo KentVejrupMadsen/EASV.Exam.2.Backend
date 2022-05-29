5426dc37b4b85fc5271b96a2bc982931966fea02
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-29 15:03:25 +0200

    added routes to recaptcha controller

--- code/routes/api/security/securityRecapApi.php
+++ code/routes/api/security/securityRecapApi.php
@@ -6,7 +6,31 @@
      */
 
     use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\http\additional\SecurityRecaptchaController;
 
 
+    //
+    Route::get(
+        '/1.0.0/security/recaptcha/read',
+        [ SecurityRecaptchaController::class, 'read' ]
+    );
+
+    // Create
+    Route::post(
+        '/1.0.0/security/recaptcha/create',
+        [ SecurityRecaptchaController::class, 'create' ]
+    );
+
+    // Update
+    Route::patch(
+        '/1.0.0/security/recaptcha/update',
+        [ SecurityRecaptchaController::class, 'update' ]
+    );
+
+    // Delete
+    Route::delete(
+        '/1.0.0/security/recaptcha/delete',
+        [ SecurityRecaptchaController::class, 'delete' ]
+    );
 
 ?>
