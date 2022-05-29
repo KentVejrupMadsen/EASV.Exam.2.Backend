b62057745f5b67e9818c7ffafb9255a38ed9c8f8
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-29 13:59:35 +0200

    current

--- code/routes/api/account/accountApi.php
+++ code/routes/api/account/accountApi.php
@@ -9,6 +9,8 @@
     use App\Http\Controllers\http\AccountController;
     use Illuminate\Support\Facades\Route;
 
+    require_once 'newsletterApi.php';
+
 
     //
     Route::post(
---
+++ code/routes/api/account/entities/personApi.php
@@ -0,0 +1,5 @@
+<?php
+    use App\Http\Controllers\http\AccountController;
+    use Illuminate\Support\Facades\Route;
+
+?>
