7daecb1b46fd548d5e3343480fe07847f1b761e0
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-29 18:23:49 +0200

    added details to the home explorer

--- code/app/Http/Controllers/ApiHomeController.php
+++ code/app/Http/Controllers/ApiHomeController.php
@@ -1,20 +1,104 @@
 <?php
+    /**
+     * Author: Kent vejrup Madsen
+     * Contact: Kent.vejrup.madsen@protonmail.com
+     * Description:
+     * TODO: Make description
+     */
     namespace App\Http\Controllers;
 
+    // external libraries
     use Illuminate\Http\Request;
 
 
-
+    //
     class ApiHomeController
         extends Controller
     {
-        //
-        public function home()
+        protected function currentVersionNumber(): string
         {
-            $response = array();
+            return '1.0.0';
+        }
 
-            $response[ 'explorer' ] = array();
+        protected function currentVersion(): string
+        {
+            return 'Version ' . $this->currentVersionNumber() . ' Alpha';
+        }
 
+        protected function generateAccountApi(): array
+        {
+            $structure = [];
+
+            return $structure;
+        }
+
+        protected function generateOptionsApi(): array
+        {
+            $structure = [];
+
+            return $structure;
+        }
+
+        protected function generateSecurityApi(): array
+        {
+            $structure = [];
+
+            return $structure;
+        }
+
+        protected function generateToolsApi(): array
+        {
+            $structure = [];
+
+            return $structure;
+        }
+
+        protected function generateExplorerStructure(): array
+        {
+            $structure =
+            [
+                'current' => $this->currentVersion(),
+                'paths'   =>
+                [
+                    $this->currentVersionNumber() =>
+                    [
+                        'account'   => $this->generateAccountApi(),
+                        'options'   => $this->generateOptionsApi(),
+                        'security'  => $this->generateSecurityApi(),
+                        'tools'     => $this->generateToolsApi()
+                    ]
+                ]
+            ];
+
+            return $structure;
+        }
+
+        /**
+         * @return string
+         */
+        protected function apiUrl(): string
+        {
+            return './api';
+        }
+
+        /**
+         * @return array
+         */
+        protected function generateApiExplorer(): array
+        {
+            $returnValue = array();
+
+            $returnValue[ 'root' ] = $this->apiUrl();
+            $returnValue[ 'explorer' ] = $this->generateExplorerStructure();
+
+            return $returnValue;
+        }
+
+
+        // Response
+        public final function home()
+        {
+            $response = $this->generateApiExplorer();
             return Response()->json( $response );
         }
 
