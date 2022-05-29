d42a72aa0183fc982cfb612073f41dd8574820d5
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-29 14:57:32 +0200

    done with route name api

--- code/routes/api/account/entities/emailApi.php
+++ code/routes/api/account/entities/emailApi.php
@@ -1,9 +1,34 @@
 <?php
-    /**
-     * Author: Kent vejrup Madsen
-     * Description:
-     * TODO: Make description
-     */
+/**
+ * Author: Kent vejrup Madsen
+ * Description:
+ * TODO: Make description
+ */
     use Illuminate\Support\Facades\Route;
 
+    use App\Http\Controllers\http\account\entities\PersonEmailController;
+
+
+    Route::get(
+        '/1.0.0/account/entities/email/read',
+        [ PersonEmailController::class, 'read' ]
+    );
+
+    // Create
+    Route::post(
+        '/1.0.0/account/entities/email/create',
+        [ PersonEmailController::class, 'create' ]
+    );
+
+    // Update
+    Route::patch(
+        '/1.0.0/account/entities/email/update',
+        [ PersonEmailController::class, 'update' ]
+    );
+
+    // Delete
+    Route::delete(
+        '/1.0.0/account/entities/email/delete',
+        [ PersonEmailController::class, 'delete' ]
+    );
 ?>
