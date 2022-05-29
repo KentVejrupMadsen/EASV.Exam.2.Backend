0888f973770f68940d65aa1dcafcb1c624e7b862
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-29 15:08:51 +0200

    adding routes to controllers

--- code/routes/api/tool/kanbanApi.php
+++ code/routes/api/tool/kanbanApi.php
@@ -9,4 +9,27 @@
     use Illuminate\Support\Facades\Route;
     use App\Http\Controllers\http\tools\KanbanController;
 
+
+    Route::get(
+        '/1.0.0/tool/kanban/read',
+        [ KanbanController::class, 'read' ]
+    );
+
+    // Create
+    Route::post(
+        '/1.0.0/tool/kanban/create',
+        [ KanbanController::class, 'create' ]
+    );
+
+    // Update
+    Route::patch(
+        '/1.0.0/tool/kanban/update',
+        [ KanbanController::class, 'update' ]
+    );
+
+    // Delete
+    Route::delete(
+        '/1.0.0/tool/kanban/delete',
+        [ KanbanController::class, 'delete' ]
+    );
 ?>
--- code/routes/api/tool/projectApi.php
+++ code/routes/api/tool/projectApi.php
@@ -9,4 +9,27 @@
     use Illuminate\Support\Facades\Route;
     use App\Http\Controllers\http\tools\ProjectController;
 
+
+    Route::get(
+        '/1.0.0/tool/project/read',
+        [ ProjectController::class, 'read' ]
+    );
+
+    // Create
+    Route::post(
+        '/1.0.0/tool/project/create',
+        [ ProjectController::class, 'create' ]
+    );
+
+    // Update
+    Route::patch(
+        '/1.0.0/tool/project/update',
+        [ ProjectController::class, 'update' ]
+    );
+
+    // Delete
+    Route::delete(
+        '/1.0.0/tool/project/delete',
+        [ ProjectController::class, 'delete' ]
+    );
 ?>
