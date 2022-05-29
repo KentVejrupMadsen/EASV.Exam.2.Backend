af29af81db558200336eaa706d745389abffd23a
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-29 17:59:47 +0200

    commenting

--- code/routes/channels.php
+++ code/routes/channels.php
@@ -1,12 +1,14 @@
 <?php
     /**
      * Author: Kent vejrup Madsen
+     * Contact: Kent.vejrup.madsen@protonmail.com
      * Description:
      * TODO: Make description
      */
+    // External libraries
     use Illuminate\Support\Facades\Broadcast;
 
-
+    //
     Broadcast::channel( 'App.Models.User.{id}', 
         function( $user, $id ) 
         {
