2b7e9a74f1640a12f7f9f50a71df2c0953ae8b1d
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-29 20:27:13 +0200

    removing unnecesarry code

---
+++ code/app/Http/Controllers/templates/ControllerOption.php
@@ -0,0 +1,17 @@
+<?php
+    namespace App\Http\Controllers\templates;
+
+    use App\Http\Controllers\Controller;
+
+
+    abstract class ControllerOption
+        extends Controller
+    {
+        public function __construct()
+        {
+
+        }
+
+
+    }
+?>
