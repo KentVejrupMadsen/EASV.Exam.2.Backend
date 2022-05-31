03daaafc4cb31c68cd6e5ccefda807be31e37905
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-31 16:45:21 +0200

    filling view models

--- code/app/Models/views/PersonNameViewModel.php
+++ code/app/Models/views/PersonNameViewModel.php
@@ -16,28 +16,34 @@
     class PersonNameViewModel
         extends ModelView
     {
-
         public $timestamps = false;
         protected $table = 'person_names_view';
 
+
         protected $fillable =
-            [
-                '',
-                '',
-                ''
-            ];
+        [
+            'id',
+            'account_information_id',
+            'person_first_name',
+            'person_name_middlename',
+            'person_last_name'
+        ];
 
         protected $hidden =
-            [
-                '',
-            ];
+        [
+            'id',
+            'account_information_id'
+        ];
 
 
         protected $casts =
-            [
-                ''    => 'integer',
-                ''  => 'datetime',
-                ''  => 'datetime',
-            ];
+        [
+            'id' => 'integer',
+            'account_information_id' => 'integer',
+
+            'person_first_name'      => 'string',
+            'person_name_middlename' => 'array',
+            'person_last_name'       => 'string'
+        ];
     }
 ?>
