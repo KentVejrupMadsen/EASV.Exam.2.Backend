064911d0359be4d6767133fa945b0b89b89692be
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-05-31 13:12:36 +0200

    normalising project route

--- code/routes/api/tool/projectApi.php
+++ code/routes/api/tool/projectApi.php
@@ -11,34 +11,35 @@
     // Internal libraries
     use App\Http\Controllers\httpControllers\tools\ProjectController;
 
-    const boardRoute = '/' . CURRENT_VERSION . '/tool/board';
-    const boardReadRoute = boardRoute . '/read';
-    const boardCreateRoute = boardRoute . '/create';
-    const boardUpdateRoute = boardRoute . '/update';
-    const boardDeleteRoute = boardRoute . '/delete';
+    const projectRoute = '/' . CURRENT_VERSION . '/tool/project';
 
-    
+    const projectReadRoute = projectRoute . '/read';
+    const projectCreateRoute = projectRoute . '/create';
+    const projectUpdateRoute = projectRoute . '/update';
+    const projectDeleteRoute = projectRoute . '/delete';
+
+
     // Routes
     Route::get(
-        '/1.0.0/tool/project/read',
+        projectReadRoute,
         [ ProjectController::class, 'read' ]
     );
 
         // Create
     Route::post(
-        '/1.0.0/tool/project/create',
+        projectCreateRoute,
         [ ProjectController::class, 'create' ]
     );
 
         // Update
     Route::patch(
-        '/1.0.0/tool/project/update',
+        projectUpdateRoute,
         [ ProjectController::class, 'update' ]
     );
 
         // Delete
     Route::delete(
-        '/1.0.0/tool/project/delete',
+        projectDeleteRoute,
         [ ProjectController::class, 'delete' ]
     );
 ?>
