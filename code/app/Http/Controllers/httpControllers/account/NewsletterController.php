0b45faf5ab9fe783f117fa2080ae75f4e6e55963
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-01 19:55:56 +0200

    removing boilerplate code

--- code/app/Http/Controllers/httpControllers/account/NewsletterController.php
+++ code/app/Http/Controllers/httpControllers/account/NewsletterController.php
@@ -10,6 +10,7 @@
     // External Libraries
     use Carbon\Carbon;
 
+    use Illuminate\Http\JsonResponse;
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Auth;
     use Illuminate\Support\Facades\Hash;
@@ -81,193 +82,55 @@
             return null;
         }
 
-        // Crud
         /**
-         * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+         * @param Request $request
+         * @return JsonResponse
          */
-        #[OA\Get(path: '/api/data.json')]
+        #[OA\Get( path: '/api/1.0.0/accounts/newsletter/read' )]
         #[OA\Response(response: '200', description: 'The data')]
-        public final function me()
+        public final function read( Request $request ): JsonResponse
         {
-            $currentUser = Auth::user();
-            return response( $currentUser );
-        }
 
-
-        /**
-         * @return void
-         */
-        #[OA\Get(path: '/api/data.json')]
-        #[OA\Response(response: '200', description: 'The data')]
-        public final function read( Request $request )
-        {
-            
+            return Response()->json();
         }
 
 
         /**
          * @param Request $request
-         * @return \Illuminate\Http\JsonResponse
+         * @return JsonResponse
          */
-        #[OA\Get(path: '/api/data.json')]
-        #[OA\Response(response: '200', description: 'The data')]
-        public final function login( Request $request )
-        {
-            $this->CSRFTokenController->access( $request );
-
-            $account_information = $request->input( 'account' );
-
-
-            if( Auth::attempt( [ 'username'=> Str::lower( $account_information[ 'username' ] ),
-                                 'password' => $account_information['security']['password'] ] ) )
-            {
-                $author = Auth::user();
-
-                $token = $author->createToken( 'account' )->plainTextToken;
-
-                $auth = array();
-                $auth['authorised'] = ['token_bearer' => $token, 'username' => $author->username ];
-
-                return response()->json( $auth, 200 );
-            }
-            else 
-            {
-                return response()->json( 'Unauthorised.', [ 'error'=>'Unauthorised' ] );
-            }
-        }
-
-
-        /**
-         * @param Request $request
-         * @return void
-         */
-        #[OA\Get(path: '/api/data.json')]
-        #[OA\Response(response: '200', description: 'The data')]
-        public final function logout( Request $request )
-        {
-            $request->user()
-                    ->currentAccessToken()
-                    ->delete();
-        }
-
-
-        /**
-         * @param Request $request
-         * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
-         */
-        #[OA\Get(path: '/api/data.json')]
+        #[OA\Post(path: '/api/1.0.0/accounts/newsletter/create')]
         #[OA\Response(response: '200', description: 'The data')]
         public final function create( Request $request )
         {
-            $this->CSRFTokenController->access( $request );
 
-            $account_information = $request->input( 'account' );
-
-            $email_str = $account_information[ 'person' ][ 'email' ];
-
-            $mail = $this->EmailModelController->find( $request );
-
-            if( is_null( $mail ) )
-            {
-                $mail = $this->EmailModelController->create( $request );
-            }
-
-            $account_create_fields = array();
-            $account_create_fields[ 'email_id' ]  = $mail->id;
-
-            $account_create_fields[ 'username' ]  = $account_information[ 'username' ];
-            $account_create_fields[ 'password' ]  = Hash::make( $account_information[ 'security' ][ 'password' ] );
-
-            $account = User::create( $account_create_fields );
-
-            return response( $account, 200 );
+            return Response()->json(null, 200);
         }
 
 
         /**
          * @param Request $request
-         * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+         * @return JsonResponse
          */
-        #[OA\Get(path: '/api/data.json')]
+        #[OA\Patch(path: '/api/1.0.0/accounts/newsletter/update')]
         #[OA\Response(response: '200', description: 'The data')]
-        public final function update( Request $request )
+        public final function update( Request $request ): JsonResponse
         {
-            $this->CSRFTokenController->access( $request );
 
-            $account = Auth::user();
-
-            $response = array();
-            $accountInformation = $request->input('account');
-
-
-            $mailModel = $this->EmailModelController->find( $accountInformation[ 'person_email' ] );
-
-            if( is_null( $mailModel ) )
-            {
-                $mailModel = $this->EmailModelController->create( $accountInformation[ 'person_email' ] );
-            }
-
-            $account->email_id = $mailModel->id;
-
-            // Passwords
-            $new = Hash::make($accountInformation['security']['password']);
-            $account->password = $new;
-
-            $account->save();
-
-            return Response($response, 200);
+            return Response()->json(null, 200);
         }
 
 
         /**
          * @param Request $request
-         * @return void
+         * @return JsonResponse
          */
-        #[OA\Get(path: '/api/data.json')]
+        #[OA\Delete(path: '/api/1.0.0/accounts/newsletter/delete')]
         #[OA\Response(response: '200', description: 'The data')]
-        public final function delete( Request $request )
+        public final function delete( Request $request ): JsonResponse
         {
-            $this->CSRFTokenController->access( $request );
 
-            $response = array();
-
-            $account = Auth::user();
-
-            $account->delete();
-            $response[ 'message' ] = 'successful' ;
-
-            return Response( $response, 200 );
-        }
-
-
-        /**
-         * @param Request $request
-         * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
-         */
-        #[OA\Get(path: '/api/data.json')]
-        #[OA\Response(response: '200', description: 'The data')]
-        public final function verify( Request $request )
-        {
-            $this->CSRFTokenController->access( $request );
-
-            $response = array();
-
-            $user = Auth::user();
-
-            if( is_null( $user->email_verified_at ) )
-            {
-                $user->email_verified_at = Carbon::now();
-                $user->save();
-
-                $response['message'] = 'verified';
-                $response['at']= $user->email_verified_at;
-            }
-            else
-            {
-                abort(self::conflict );
-            }
-
-            return Response( $response );
+            return Response()->json(null, 200);
         }
 
         private static $controller = null;
