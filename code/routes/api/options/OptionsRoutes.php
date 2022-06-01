f8be91ec6b9245e333b6f9bc8d7c40c417afe065
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-01 10:33:04 +0200

    organising code and streamlining it

--- code/routes/api/options/OptionsRoutes.php
+++ code/routes/api/options/OptionsRoutes.php
@@ -19,8 +19,8 @@
         (
             function()
             {
-                findApi();
-                stateApi();
+                FindApi();
+                StateApi();
             }
         );
     }
