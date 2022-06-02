4aa2262c42bd51966c8d8d3de94778f25c4581de
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-02 13:57:36 +0200

    c

--- code/app/Http/Controllers/OpenApi.php
+++ code/app/Http/Controllers/OpenApi.php
@@ -10,14 +10,12 @@
     use OpenApi\Attributes
         as OA;
 
-    define( 'APP_URL', env('APP_URL') );
-
     #[OA\Info( version: '1.0.0-alpha',
                title: 'kanban_project_backend_api' ) ]
     #[OA\Contact( name: 'kent vejrup madsen',
                   url: 'https://github.com/KentVejrupMadsen',
                   email: 'kent.vejrup.madsen@protonmail.com')]
-    #[OA\Server( url: APP_URL )]
+    #[OA\Server( url: 'https://kanban.goalpioneers.com/api/1.0.0' )]
     #[OA\License( name: 'MIT License',
                   url: 'https://github.com/KentVejrupMadsen/Kanban-Project-Backend/blob/main/License.md' )]
     final class OpenApi
