17fa46c61045fbbe489d98116167301aee3335b6
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-09 12:57:22 +0200

    formating code

--- code/app/Views/AddressViewModel.php
+++ code/app/Views/AddressViewModel.php
@@ -23,30 +23,69 @@
     class AddressViewModel
         extends ModelView
     {
-        #[OA\Property( title: '',  type: 'string' )]
+        #[OA\Property( title: '',
+                       type: 'string',
+                       readOnly: true,
+                       writeOnly: false,
+                       deprecated: false )]
         public const table_name = 'addresses_view';
         protected $table  = self::table_name;
 
 
-        #[OA\Property( title: '',  type: 'string', readonly: true, writeOnly: false, deprecated: false )]
+        #[OA\Property( title: '',
+                       type: 'string',
+                       readonly: true,
+                       writeOnly: false,
+                       nullable: false,
+                       deprecated: false )]
         protected const field_id = 'id';
 
-        #[OA\Property( title: '',  type: 'string', readonly: true, writeOnly: false, deprecated: false )]
+        #[OA\Property( title: '',
+                       type: 'string',
+                       readonly: true,
+                       writeOnly: false,
+                       nullable: false,
+                       deprecated: false )]
         protected const field_account_information_id = 'account_information_id';
 
-        #[OA\Property( title: '',  type: 'string', readonly: true, writeOnly: false, deprecated: false )]
+        #[OA\Property( title: '',
+                       type: 'string',
+                       readonly: true,
+                       writeOnly: false,
+                       nullable: false,
+                       deprecated: false )]
         protected const field_road_name = 'road_name';
 
-        #[OA\Property( title: '',  type: 'string', readonly: true, writeOnly: false, deprecated: false )]
+        #[OA\Property( title: '',
+                       type: 'string',
+                       readonly: true,
+                       writeOnly: false,
+                       nullable: false,
+                       deprecated: false )]
         protected const field_road_number = 'road_number';
 
-        #[OA\Property( title: '',  type: 'string', readonly: true, writeOnly: false, deprecated: false )]
+        #[OA\Property( title: '',
+                       type: 'string',
+                       readonly: true,
+                       writeOnly: false,
+                       nullable: false,
+                       deprecated: false )]
         protected const field_levels = 'levels';
 
-        #[OA\Property( title: '',  type: 'string', readonly: true, writeOnly: false, deprecated: false )]
+        #[OA\Property( title: '',
+                       type: 'string',
+                       readonly: true,
+                       writeOnly: false,
+                       nullable: false,
+                       deprecated: false )]
         protected const field_address_country = 'address_country';
 
-        #[OA\Property( title: '',  type: 'string', readonly: true, writeOnly: false, deprecated: false )]
+        #[OA\Property( title: '',
+                       type: 'string',
+                       readonly: true,
+                       writeOnly: false,
+                       nullable: false,
+                       deprecated: false )]
         protected const field_zip_code_id = 'zip_code_id';
 
 
