f1f8ea57cabaaa22bd603c12caa4ebcf0c2a5fcd
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-11 13:51:05 +0200

    automated scripts

---
+++ scripts/automated.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# automated test.
+
+# Move to parent directory
+cd ..
+
+currentDirectory=$(pwd)
+
+# directory path names
+scriptsDirectory="/scripts"
+automateDirectory="/automate"
+
+# automate collection directory
+scriptCollectionDirectory="$currentDirectory$scriptsDirectory$automateDirectory"
+echo "$scriptCollectionDirectory"
+
+echo "running automated bash scripts"
+for file in "$scriptCollectionDirectory"/*.sh; do
+  script="$(basename "$file")"
+  full_script_path="$scriptCollectionDirectory/$script"
+
+  bash "$full_script_path"
+done
