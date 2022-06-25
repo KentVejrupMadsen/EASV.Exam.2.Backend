acbd86bf5c43118016982906e202981738693c22
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-25 19:52:56 +0200

    adding license

--- code/database/seeders/DatabaseDefaultValuesSeeder.php
+++ code/database/seeders/DatabaseDefaultValuesSeeder.php
@@ -1,23 +1,21 @@
 <?php
-    /*
+    /**
      * Author: Kent vejrup Madsen
      * Contact: Kent.vejrup.madsen@designermadsen.com
      * Description:
      * Tags: 
-     * License: MIT License (https://opensource.org/licenses/MIT)
+     * License: MIT License
+        * (https://github.com/KentVejrupMadsen/EASV.Exam.2.Backend/blob/main/License.md)
+        * (https://opensource.org/licenses/MIT)
      * Copyright: Kent vejrup Madsen, 2022
      */
     namespace Database\Seeders;
 
     use App\Models\tables\AddressModel;
     use App\Models\tables\CountryModel;
-    use App\Models\tables\KanbanTitleModel;
-    use App\Models\tables\MemberGroupModel;
     use App\Models\tables\PersonFirstnameModel;
     use App\Models\tables\PersonSurnameModel;
-    use App\Models\tables\ProjectTitleModel;
     use App\Models\tables\ZipCodeModel;
-    use App\Models\tables\BoardTitleModel;
 
     use Illuminate\Database\Console\Seeds\WithoutModelEvents;
     use Illuminate\Database\Seeder;
@@ -29,13 +27,8 @@
         public function run()
         {
             //
-            BoardTitleModel::factory()->create( [ 'content' => 'unknown' ] );
             PersonFirstnameModel::factory()->create( [ 'content' => 'unknown' ] );
             PersonSurnameModel::factory()->create( [ 'content' => 'unknown' ] );
-            ProjectTitleModel::factory()->create( [ 'content' => 'unknown' ] );
-            MemberGroupModel::factory()->create( [ 'content' => 'unknown' ] );
-            KanbanTitleModel::factory()->create( [ 'content' => 'unknown' ] );
-            BoardTitleModel::factory()->create( [ 'content' => 'unknown' ] );
             AddressModel::factory()->create( [ 'content' => 'unknown' ] );
 
             $C = CountryModel::factory()->create( [ 'country_name' => 'unknown',
