91ed9bc1bb44b8eb04cfb2d33fb0c351302ef22d
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-07 12:38:28 +0200

    current

---
+++ code/database/migrations/2022_06_25_214402_robottxt_migration.php
@@ -0,0 +1,39 @@
+<?php
+    /**
+     * Author: Kent vejrup Madsen
+     * Contact: Kent.vejrup.madsen@designermadsen.com
+     * Description:
+     * Tags:
+     * License: MIT License
+        * (https://github.com/KentVejrupMadsen/EASV.Exam.2.Backend/blob/main/License.md)
+        * (https://opensource.org/licenses/MIT)
+     * Copyright: Kent vejrup Madsen, 2022
+     */
+	use Illuminate\Database\Migrations\Migration;
+	use Illuminate\Database\Schema\Blueprint;
+	use Illuminate\Support\Facades\Schema;
+	
+	
+	return new class extends Migration
+	{
+	    /**
+	     * Run the migrations.
+	     *
+	     * @return void
+	     */
+	    public function up()
+	    {
+	        //
+	    }
+	
+	    /**
+	     * Reverse the migrations.
+	     *
+	     * @return void
+	     */
+	    public function down()
+	    {
+	        //
+	    }
+	};
+?>
